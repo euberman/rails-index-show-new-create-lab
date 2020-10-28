f901ecf7fdfc7a144d1069b8a1aa372671ae740d
euberman <euberman@gmail.com>
2020-10-28 11:51:38 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create([
+  { coupon_code: 'jdkdoik', store: 'Costco' },
+  { coupon_code: 'gdsafsd', store: 'Sams Club' },
+  { coupon_code: 'ddfdsa', store: 'Walmart' },
+  { coupon_code: 'adfasdf', store: 'Office Depot' }
+])
